332fa2025d5b6f0ba3359ca5c5c66244a52fccc2
meldeeb <mohammad.edeeb@gmail.com>
2017-12-09 05:58:48 +0200

    Remove seed data

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,7 +1,7 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :challenge
-  has_many :blocks, inverse_of: :subscription
+  has_many :blocks, dependent: :destroy, inverse_of: :subscription
 
   accepts_nested_attributes_for :blocks
 end
